0d3a457f7828e2eff5f49a3c22229f838432598f
lisalee-123 <lisalee.dev@gmail.com>
2023-05-03 11:13:05 +0200

    added pokemon data to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert("Hello world");
-let favouriteFood = "Spaghetti Bolognese";
-document.write(favouriteFood);
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 7,
+    types: ["Monster", "Grass"],
+  },
+  {
+    name: "Charmander",
+    height: 0.6,
+    types: ["Fire"],
+  },
+  {
+    name: "Caterpie",
+    height: 0.3,
+    types: ["Bug"],
+  },
+];
