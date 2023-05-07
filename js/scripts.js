15a1fdf371d9312e89b1bcb0abbe4eb582fbc777
lisalee-123 <lisalee.dev@gmail.com>
2023-05-07 09:22:54 +0200

    added styling of Navbar, semantic tags and 2 Logos

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,44 @@ let pokemonList = [
   },
 ];
 
-for (i = 0; i < pokemonList.length; i++) {
-  document.write(
-    pokemonList[i].name + " (height: " + pokemonList[i].height + ") </br>" //html within string to write in a new line
-  ); //loop through all pokemons and add text & spacing
-  if (pokemonList[i].height > 0.6) {
-    //conditional statemement added
-    document.write(" - Wow, that's big!</br> </br>");
+let pokemonList2 = [
+  {
+    name: "Pidgey",
+    height: 0.3,
+    types: ["Flying", "Normal"],
+  },
+  {
+    name: "Rattata",
+    height: 0.3,
+    types: ["Fire"],
+  },
+  {
+    name: "Ekans",
+    height: 2,
+    types: ["Poison"],
+  },
+];
+
+document.write("<div><h1> Pokemon </h1></div>");
+
+function printArrayDetails(list) {
+  //list = function parameter => placeholder
+  for (let i = 0; i < list.length; i++) {
+    document.write(
+      "<div class='pokemonList_Item'><p>" +
+        list[i].name +
+        " (height: " +
+        list[i].height +
+        "m) </p></div>" //html within string to display as paragraph
+    ); //loop through all pokemons and add text & spacing
+    if (list[i].height > 0.6) {
+      //conditional statemement added
+      document.write(
+        "<div class='pokemonList_Item'><p> - Wow, that's big! </p></div>"
+      );
+    }
   }
 }
+
+printArrayDetails(pokemonList2); // pokemonList2 is now an argument that gets passed onto the funtion
+printArrayDetails(pokemonList); // list in the function becomes the argument I put in
