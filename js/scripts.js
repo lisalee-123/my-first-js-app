86101b0828fdb47e78c510f292ec4e00866de7f8
lisalee-123 <lisalee.dev@gmail.com>
2023-05-08 09:34:00 +0200

    refactor: replaced the for loop with a forEach loop in the PrintArrayDetails function

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,68 @@
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["Monster", "Grass"],
-  },
-  {
-    name: "Charmander",
-    height: 0.6,
-    types: ["Fire"],
-  },
-  {
-    name: "Caterpie",
-    height: 0.3,
-    types: ["Bug"],
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["Monster", "Grass"],
+    },
+    {
+      name: "Charmander",
+      height: 0.6,
+      types: ["Fire"],
+    },
+    {
+      name: "Caterpie",
+      height: 0.3,
+      types: ["Bug"],
+    },
+    {
+      name: "Pidgey",
+      height: 0.3,
+      types: ["Flying", "Normal"],
+    },
+    {
+      name: "Rattata",
+      height: 0.3,
+      types: ["Fire"],
+    },
+    {
+      name: "Ekans",
+      height: 2,
+      types: ["Poison"],
+    },
+  ];
+  function getAll() {
+    return pokemonList;
+  }
+  function add() {
+    pokemonList.push(pokemon);
+  }
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
 
-let pokemonList2 = [
-  {
-    name: "Pidgey",
-    height: 0.3,
-    types: ["Flying", "Normal"],
-  },
-  {
-    name: "Rattata",
-    height: 0.3,
-    types: ["Fire"],
-  },
-  {
-    name: "Ekans",
-    height: 2,
-    types: ["Poison"],
-  },
-];
+console.log(pokemonRepository.getAll());
 
 document.write("<div><h1> Pokemon </h1></div>");
 
 function printArrayDetails(list) {
-  //list = function parameter => placeholder
-  for (let i = 0; i < list.length; i++) {
+  list.forEach(function (pokemon) {
+    //pokemon argument replaces the list[i]
     document.write(
       "<div class='pokemonList_Item'><p>" +
-        list[i].name +
+        pokemon.name +
         " (height: " +
-        list[i].height +
-        "m) </p></div>" //html within string to display as paragraph
-    ); //loop through all pokemons and add text & spacing
-    if (list[i].height > 0.6) {
-      //conditional statemement added
+        pokemon.height +
+        "m) </p></div>"
+    );
+    if (pokemon.height > 0.6) {
       document.write(
         "<div class='pokemonList_Item'><p> - Wow, that's big! </p></div>"
       );
     }
-  }
+  });
 }
 
-printArrayDetails(pokemonList2); // pokemonList2 is now an argument that gets passed onto the funtion
-printArrayDetails(pokemonList); // list in the function becomes the argument I put in
+printArrayDetails(pokemonRepository.getAll()); // list in the function becomes the argument I put in
